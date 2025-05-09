1ae4c14fd3d7ea9156eb3cefa80a5074d9bff4d3
Sugandhan <asugadhanslmn@example.com>
2025-05-09 18:14:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+ import ConverterForm from "./Components/ConverterForm";
+ const App = () => {
+   return (
+     <div className="currency-converter">
+       <h2 className="converter-title">Currency Converter</h2>
+       <ConverterForm />
+     </div>
+   )
+ }
+export default App;
+
+// import Students from './Study/Students.jsx'
+
+// function App()
+// {
+//   return(   
+//     <>
+//         <Students isLogedin={false} username="Sugandhan"/>
+//     </>
+//   );
+// }
+
+// export default App;
